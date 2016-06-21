2163953f10751284350430a0e7d7e8cb65103905
hudson-prestidge <charcon@ihug.co.nz>
2016-06-21 19:46:52 +1200

    implement addlisteners(), showcell()

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 function startGame () {
+  var squares = document.getElementsByClassName('board')[0].children
+  console.log(squares)
+  for (var i = 0; i < squares.length; i++) {
+    addListeners(squares[i])
+  }
+}
+
+function addListeners (element) {
+  element.addEventListener('click', showCell)
+}
+
+function showCell (evt) {
+  evt.target.classList.remove('hidden')
 }
